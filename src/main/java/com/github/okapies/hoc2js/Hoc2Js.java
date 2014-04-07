6f499a20dc779956cab7e48a16b071f19374fc25
okapies <okapies@gmail.com>
2014-04-07 23:07:27 +0900

    make hoc2js configurable by application.conf

--- src/main/java/com/github/okapies/hoc2js/Hoc2Js.java
+++ src/main/java/com/github/okapies/hoc2js/Hoc2Js.java
@@ -3,44 +3,94 @@ package com.github.okapies.hoc2js;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
-import java.io.Reader;
 
-import com.typesafe.config.Config;
-import com.typesafe.config.ConfigFactory;
-import com.typesafe.config.ConfigParseOptions;
-import com.typesafe.config.ConfigSyntax;
-import com.typesafe.config.ConfigRenderOptions;
-import com.typesafe.config.ConfigResolveOptions;
+import com.typesafe.config.*;
 
 /**
  * Very simple utility for converting from Typesafe's HOCON to JSON.
  * See https://github.com/typesafehub/config/blob/master/HOCON.md
  */
 public class Hoc2Js {
-	public static void main(String[] args) {
-		Config config = ConfigFactory.empty();
+    public static void main(String[] args) {
+        Config config = ConfigFactory.load();
 
-		// parse
-		ConfigParseOptions parseOpts = ConfigParseOptions.defaults()
-			.setSyntax(ConfigSyntax.CONF); // HOCON
-		if (args.length > 0) {
-			for (String filename : args) {
-				config = config.withFallback(ConfigFactory.parseFile(new File(filename)));
-			}
-		} else {
-			// read from stdin instead of .conf files
-			config = ConfigFactory.parseReader(
-				new BufferedReader(new InputStreamReader(System.in)));
-		}
+        Config obj = ConfigFactory.empty();
 
-		// resolve
-		ConfigResolveOptions resolveOpts = ConfigResolveOptions.defaults();
-		config = config.resolve(resolveOpts);
+        // parse
+        ConfigParseOptions parseOpts = loadParseOpts(config);
+        if (args.length > 0) {
+            for (String filename : args) {
+                obj = obj.withFallback(ConfigFactory.parseFile(new File(filename), parseOpts));
+            }
+        } else {
+            // read from stdin instead of .conf files
+            obj = ConfigFactory.parseReader(
+                new BufferedReader(new InputStreamReader(System.in)), parseOpts);
+        }
 
-		// render
-		ConfigRenderOptions renderOpts = ConfigRenderOptions.defaults()
-			.setOriginComments(false)
-			.setOriginComments(false);
-		System.out.print(config.root().render(renderOpts));
-	}
+        // resolve
+        ConfigResolveOptions resolveOpts = loadResolveOpts(config);
+        obj = obj.resolveWith(config, resolveOpts);
+
+        // render
+        ConfigRenderOptions renderOpts = loadRenderOpts(config);
+        System.out.print(obj.root().render(renderOpts));
+    }
+
+    private static ConfigParseOptions loadParseOpts(Config config) {
+        ConfigSyntax syntax = getNullableConfigSyntax(config, "hoc2js.parse.syntax");
+        String originDescription = getNullableString(config, "hoc2js.parse.originDescription");
+        boolean allowMissing = config.getBoolean("hoc2js.parse.allowMissing");
+
+        return ConfigParseOptions.defaults()
+            .setSyntax(syntax)
+            .setOriginDescription(originDescription)
+            .setAllowMissing(allowMissing);
+    }
+
+    private static ConfigResolveOptions loadResolveOpts(Config config) {
+        boolean useSystemEnvironment = config.getBoolean("hoc2js.resolve.useSystemEnvironment");
+        boolean allowUnresolved = config.getBoolean("hoc2js.resolve.allowUnresolved");
+
+        return ConfigResolveOptions.defaults()
+                .setUseSystemEnvironment(useSystemEnvironment)
+                .setAllowUnresolved(allowUnresolved);
+    }
+
+    private static ConfigRenderOptions loadRenderOpts(Config config) {
+        boolean originComments = config.getBoolean("hoc2js.render.originComments");
+        boolean comments = config.getBoolean("hoc2js.render.comments");
+        boolean formatted = config.getBoolean("hoc2js.render.formatted");
+        boolean json = config.getBoolean("hoc2js.render.json");
+
+        return ConfigRenderOptions.defaults()
+                .setOriginComments(originComments)
+                .setComments(comments)
+                .setFormatted(formatted)
+                .setJson(json);
+    }
+
+    private static String getNullableString(Config config, String path) {
+        String ret;
+
+        try {
+            ret = config.getString(path);
+        } catch (ConfigException.Missing e) {
+            ret = null;
+        }
+
+        return ret;
+    }
+
+    private static ConfigSyntax getNullableConfigSyntax(Config config, String path) {
+        ConfigSyntax ret;
+
+        try {
+            ret = ConfigSyntax.valueOf(config.getString(path).toUpperCase());
+        } catch (ConfigException.Missing e) {
+            ret = null;
+        }
+
+        return ret;
+    }
 }
